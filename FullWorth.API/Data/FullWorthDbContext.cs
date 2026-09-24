f2d7345551361b79454c9f4f31efcc779ec7c2d5
RealizmModz <74314445+RealizmModz@users.noreply.github.com>
2026-09-24 10:50:47 -0600

    Configure Bills-owned transaction associations

--- FullWorth.API/Data/FullWorthDbContext.cs
+++ FullWorth.API/Data/FullWorthDbContext.cs
@@ -42,6 +42,9 @@ public sealed class FullWorthDbContext
     public DbSet<BillAlertEntity> BillAlerts =>
         Set<BillAlertEntity>();
 
+    public DbSet<BillTransactionAssociationEntity> BillTransactionAssociations =>
+        Set<BillTransactionAssociationEntity>();
+
     public DbSet<BillStatementUploadEntity> BillStatementUploads =>
         Set<BillStatementUploadEntity>();
 
@@ -82,6 +85,7 @@ public sealed class FullWorthDbContext
         ConfigureBillLineItem(builder);
         ConfigureBillChange(builder);
         ConfigureBillAlert(builder);
+        ConfigureBillTransactionAssociation(builder);
         ConfigureBillStatementUpload(builder);
         ConfigureBillStatementAiEvaluation(builder);
         ConfigurePlaidLinkSession(builder);
@@ -741,6 +745,53 @@ public sealed class FullWorthDbContext
                     })
                     .OnDelete(DeleteBehavior.Restrict);
 
+            });
+    }
+
+    private static void ConfigureBillTransactionAssociation(
+        ModelBuilder builder)
+    {
+        builder.Entity<BillTransactionAssociationEntity>(
+            entity =>
+            {
+                entity.ToTable("BillTransactionAssociations");
+
+                entity.HasKey(association => new
+                {
+                    association.UserId,
+                    association.BankTransactionId
+                });
+
+                entity.Property(association => association.CreatedAtUtc)
+                    .IsRequired();
+
+                entity.Property(association => association.UpdatedAtUtc)
+                    .IsRequired();
+
+                entity.HasIndex(association => new
+                {
+                    association.BillStreamId,
+                    association.UserId
+                });
+
+                entity.HasOne(association => association.User)
+                    .WithMany()
+                    .HasForeignKey(association => association.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(association => association.BillStream)
+                    .WithMany()
+                    .HasForeignKey(association => new
+                    {
+                        association.BillStreamId,
+                        association.UserId
+                    })
+                    .HasPrincipalKey(stream => new
+                    {
+                        stream.Id,
+                        stream.UserId
+                    })
+                    .OnDelete(DeleteBehavior.Cascade);
             });
     }
 
